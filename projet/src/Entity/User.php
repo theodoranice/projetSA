3ffd6f3ea9164f4c55bdf78b139f86b60659bfa3
theo <theo@localhost.localdomain>
2020-04-25 13:01:53 +0000

    création entité user et role

---
+++ projet/src/Entity/User.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActif;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nomComplete;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(?string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getIsActif(): ?bool
+    {
+        return $this->isActif;
+    }
+
+    public function setIsActif(bool $isActif): self
+    {
+        $this->isActif = $isActif;
+
+        return $this;
+    }
+
+    public function getNomComplete(): ?string
+    {
+        return $this->nomComplete;
+    }
+
+    public function setNomComplete(string $nomComplete): self
+    {
+        $this->nomComplete = $nomComplete;
+
+        return $this;
+    }
+}
